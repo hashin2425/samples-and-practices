4be07ca82accf8f8be62bc4d832109cbbafd00dd
Haya <hayachannel5@gmail.com>
2024-09-08 10:08:34 +0900

    なんか作ってたやつ

--- rails-sample/db/schema.rb
+++ rails-sample/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_30_045749) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_30_060554) do
   create_table "account_models", force: :cascade do |t|
     t.text "name"
     t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "diaries", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
