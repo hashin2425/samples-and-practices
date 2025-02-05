8756d26e4f03967b5e31bfb61f7d0a0f41442cc5
Haya <hayachannel5@gmail.com>
2025-02-05 23:01:06 +0900

    chore: clear directory

--- sample-rails-all-in-docker/src/test/controllers/web/home_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Web::HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get web_home_index_url
-    assert_response :success
-  end
-end
